d2c77165184ff410be1d4048485efdaff87f5206
JULIAO MUNGUAMBE <juliaomunguambe794@gmail.com>
2026-08-10 22:38:20 +0200

    POO

--- POO/Encapsulamento/exercicio1/Exercicio1.java
+++ POO/Encapsulamento/exercicio1/Exercicio1.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
+
+class Config{
+	static int MAX_ALUNOS = 2;	
+}
+
+
 class Aluno{
 	
 	private String nome;
@@ -13,12 +21,15 @@ class Aluno{
 			this.idade = idade;
 			this.curso=curso;
 			this.media = 0;		
+		}else{
+			System.out.println("Dados invalidos. Codigo: Erro no constructor");			
 		}
 		
 
 	}
 	
 	void mostrarAluno(){
+		System.out.println("");	
 		System.out.println("Nome:"+ nome);
 		System.out.println("Idade:"+idade);
 		System.out.println("Curso:"+curso);
@@ -34,7 +45,7 @@ class Aluno{
 	public String getNome(){
 		return nome;
 		
-	}
+	} 
 	
 	public String getCurso(){
 		return curso;
@@ -84,20 +95,162 @@ class Aluno{
 	
 	
 	
+	
 }
 
+class Sistema{
+	
+	void cadastrarAluno(Aluno[] alunos,Scanner input){
+		/*
+			
+			
+	
+			int startPosition = 0;
+			boolean isFull = false;
+			for(int n = 0 ; n<Config.MAX_ALUNOS; n++){
+				if(alunos[n] == null){
+					startPosition = n;
+					isFull = false;
+					break;
+				}else {
+					isFull = true;	
+				}					
+			}		
+		
+		
+		
+		*/
+		
+
+			
+
+			
+			
+
+			for(int i =0; i<Config.MAX_ALUNOS;i++){
+				
+				
+				System.out.println("Aluno " + (i+1) );
+				System.out.println("");				
+				System.out.println("Digite o nome:");
+				String nome = input.nextLine();
+				
+				System.out.println("Digite a idade:");
+				int idade = input.nextInt();
+				
+				input.nextLine();
+				
+				System.out.println("Digite o curso:");
+				String curso = input.nextLine();
+				
+				alunos[i] = new Aluno(nome,idade,curso);
+				
+				System.out.println("Digite a nota 1:");
+				double nota1 = input.nextDouble();
+				
+				System.out.println("digite a nota 2");
+				double nota2 = input.nextDouble();
+				
+				alunos[i].setMedia(nota1,nota2);
+				
+				System.out.println("Operacao feita com sucesso!");	
+				input.nextLine();
+				
+			}
+				
+		}	
+		
+		void procurarAluno(Aluno[] alunos,Scanner input){
+			System.out.println("Digite o nome:");
+			String nome = input.nextLine();
+			Boolean encontrado = false;
+			
+			for(int i = 0; i<Config.MAX_ALUNOS ; i++){
+				
+				if(alunos[i] != null){
+					if(alunos[i].getNome().equals(nome)){
+						alunos[i].mostrarAluno();
+						encontrado = true;
+					}					
+				}
+				
+				
+
+			}
+			
+				if(encontrado == false){
+					System.out.println("Aluno nao encontrado");
+				}
+			
+		}
+		
+	
+}
 
 
 public class Exercicio1{
 	
 	public static void main(String[] args){
 		
-		Aluno aluno1 = new Aluno("juliao",19,"informatica");
-		aluno1.setMedia(16,18);
-		aluno1.setCurso("12345","Eletronica");
-		aluno1.mostrarAluno();
+		Scanner input = new Scanner(System.in);
+		Sistema sistema = new Sistema();
+		Aluno [] alunos = new Aluno[Config.MAX_ALUNOS];
 		
 		
+		int opcao = 90;
+		
+		while(opcao != 0){
+			System.out.printf("1-Cadastrar\n2-Listar\n3-Procurar\n0-Sair\n\nSelecione uma opcao:");
+			opcao = input.nextInt();
+			input.nextLine();	
+			
+			
+			switch(opcao){
+					
+					case 1:
+						System.out.println("");
+						sistema.cadastrarAluno(alunos,input);
+						break;
+					case 2:
+						System.out.println("");
+						System.out.println("Lista de alunos");
+						System.out.println("");
+						
+						Boolean temAluno = false;
+					
+						for(int n = 0;n<Config.MAX_ALUNOS;n++){
+							if(alunos[n] != null){
+								alunos[n].mostrarAluno();	
+								temAluno = true;								
+							}
+
+						}
+						if(temAluno == false){
+							System.out.println("Nenhum aluno foi cadastrado");
+						}
+						
+						System.out.println("");
+						break;
+						
+					case 3:
+						System.out.println("");
+						sistema.procurarAluno(alunos,input);
+						System.out.println("");
+						break;
+						
+					case 0:
+						System.out.println("saindo");
+						break;
+					
+					default:
+						System.out.println("Opcao invalida");
+						break;
+				}						
+		}
+		
+		
+		
+
 	}
 	
 }
